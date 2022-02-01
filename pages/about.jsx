ac1d3354c5ee34f5801cebfec3f5e334bc43b458
Barclay <barclayiversen@gmail.com>
2022-01-31 23:17:17 -0800

    change photo on about

--- pages/about.jsx
+++ pages/about.jsx
@@ -12,7 +12,7 @@ export default class About extends Component {
             <img className="absolute h-5/6 w-max opacity-30 -z-50 "  src="/rose-motif.png"/> 
             <img src="/Knives-yellow.png" className="h-72 lg:w-1/4 p-1 mx-auto my-8"/>         
             <img src="/jaxx-mask.webp"     className="h-72 lg:w-1/4 p-1 mx-auto mb-5 my-8"/>
-            <img src="/group-2-yolo.png" className="h-72 lg:w-1/4 p-1 mx-auto mb-5 my-8"/>
+            <img src="/hackjob-finished.png" className="h-72 lg:w-1/4 p-1 mx-auto mb-5 my-8"/>
             <img src="/cat-yolo-cropped.png " className="h-72 lg:w-1/4 p-1 mx-auto mb-5 my-8" /> 
           </div>
           <h1 className="text-black text-2xl font-bold px-20 py-10 text-center">Dirty Not sorry is an artist and promotion collective united by the belief that music brings us together.</h1>
