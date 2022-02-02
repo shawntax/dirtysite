f7a3a3b0f3b9782159208911ebed0f78f07ffc57
Barclay <barclayiversen@gmail.com>
2022-02-02 12:46:53 -0800

    Refactor roster to use DJ card component

--- pages/roster.jsx
+++ pages/roster.jsx
@@ -1,5 +1,6 @@
 import PageContainer from '../components/PageContainer'
-import { Box, SimpleGrid, Image, Text, Center, Link } from '@chakra-ui/react'
+import { SimpleGrid } from '@chakra-ui/react'
+import Card from '../components/Card'
 
 export default function Roster() {
   return (
@@ -9,57 +10,34 @@ export default function Roster() {
         spacing="10px"
         px="8"
       >
-        <Box bg="gray.700" borderRadius="lg"  px="4" py="5">
-          <Image mx="auto" pt="2"  borderRadius="full" src="group.jpg" alt="group" />
-            <Box px="15%">
-              <Box bg="black" mx="auto" w="auto" pb="1" mt="4" borderRadius="full">
-                <Center textColor="white" fontSize="x-large">Knives</Center>
-              </Box>
-            </Box>
-          <Center px="6" py="3" textColor="white">Lorem Ipsum dorito delicious. Illus dorus parabem.</Center>
-          <Box d="flex" justifyContent="center">
-            <Link href="https://instagram.com/djknives"> <Image src="instagram.svg" h="10" mx="3"/> </Link> <Link href="https://facebook.com/djknivesofficial" > <Image src="facebook.svg" h="10"  mx="3"/> </Link>  <Link href="https://soundcloud.com/djknivesofficial" > <Image src="soundcloud.svg" h="10"  mx="3"/> </Link>
-          </Box>
-        </Box>
-
-        <Box bg="gray.700" borderRadius="lg"  px="4" py="5">
-          <Image mx="auto" pt="2"  borderRadius="full" src="group.jpg" alt="group" />
-            <Box px="15%">
-              <Box bg="black" mx="auto" w="auto" pb="1" mt="4" borderRadius="full">
-                <Center textColor="white" fontSize="x-large">Knives</Center>
-              </Box>
-            </Box>
-          <Center px="6" py="3" textColor="white">Lorem Ipsum dorito delicious. Illus dorus parabem.</Center>
-          <Box d="flex" justifyContent="center">
-            <Link href="https://instagram.com/djknives"> <Image src="instagram.svg" h="10" mx="3"/> </Link> <Link href="https://facebook.com/djknivesofficial" > <Image src="facebook.svg" h="10"  mx="3"/> </Link>  <Link href="https://soundcloud.com/djknivesofficial" > <Image src="soundcloud.svg" h="10"  mx="3"/> </Link>
-          </Box>
-        </Box>
-
-        <Box bg="gray.700" borderRadius="lg"  px="4" py="5">
-          <Image mx="auto" pt="2"  borderRadius="full" src="group.jpg" alt="group" />
-            <Box px="15%">
-              <Box bg="black" mx="auto" w="auto" pb="1" mt="4" borderRadius="full">
-                <Center textColor="white" fontSize="x-large">Knives</Center>
-              </Box>
-            </Box>
-          <Center px="6" py="3" textColor="white">Lorem Ipsum dorito delicious. Illus dorus parabem.</Center>
-          <Box d="flex" justifyContent="center">
-            <Link href="https://instagram.com/djknives"> <Image src="instagram.svg" h="10" mx="3"/> </Link> <Link href="https://facebook.com/djknivesofficial" > <Image src="facebook.svg" h="10"  mx="3"/> </Link>  <Link href="https://soundcloud.com/djknivesofficial" > <Image src="soundcloud.svg" h="10"  mx="3"/> </Link>
-          </Box>
-        </Box>
-
-        <Box bg="gray.700" borderRadius="lg"  px="4" py="5">
-          <Image mx="auto" pt="2"  borderRadius="full" src="group.jpg" alt="group" />
-            <Box px="15%">
-              <Box bg="black" mx="auto" w="auto" pb="1" mt="4" borderRadius="full">
-                <Center textColor="white" fontSize="x-large">Knives</Center>
-              </Box>
-            </Box>
-          <Center px="6" py="3" textColor="white">Lorem Ipsum dorito delicious. Illus dorus parabem.</Center>
-          <Box d="flex" justifyContent="center">
-            <Link href="https://instagram.com/djknives"> <Image src="instagram.svg" h="10" mx="3"/> </Link> <Link href="https://facebook.com/djknivesofficial" > <Image src="facebook.svg" h="10"  mx="3"/> </Link>  <Link href="https://soundcloud.com/djknivesofficial" > <Image src="soundcloud.svg" h="10"  mx="3"/> </Link>
-          </Box>
-        </Box>
+        <Card
+          djname="Knives"
+          bio="Lorem Ipsum dorito delicious. Illus dorus parabem."
+          instaLink="djknives"
+          fbLink="djknivesofficial"
+          scLink="djknivesofficial"
+        />
+        <Card
+          djname="JaxxT"
+          bio="Lorem Ipsum dorito delicious. Illus dorus parabem."
+          instaLink="jaxxtofficial"
+          fbLink="jaxxtofficial"
+          scLink="jaxxtofficial"
+        />
+        <Card
+          djname="CatLiu"
+          bio="Lorem Ipsum dorito delicious. Illus dorus parabem."
+          instaLink="catliu"
+          fbLink="catliu"
+          scLink="catliu"
+        />
+        <Card
+          djname="Glaas"
+          bio="Lorem Ipsum dorito delicious. Illus dorus parabem."
+          instaLink="glaasmusic"
+          fbLink="glaas"
+          scLink="glaasmusic"
+        />
 
       </SimpleGrid>
     </PageContainer>
