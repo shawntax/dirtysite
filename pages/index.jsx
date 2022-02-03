2d2d3775b7c0ca8bc03758a832ebf36f3c46df4b
Barclay <barclayiversen@gmail.com>
2022-02-02 21:50:59 -0800

    Bring back attributes to home page

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,9 +1,17 @@
 import PageContainer from '../components/PageContainer'
-import { Box } from '@chakra-ui/react'
+import { Container, Center } from '@chakra-ui/react'
+import { attributes, react as HomeContent } from '../content/pages/home.md'
+
 export default function Home() {
+  let { hero_title, hero_subtitle } = attributes
   return (
-    <PageContainer title="Home">
-      <Box w="100" h="100vh" bg="black"></Box>
+    <PageContainer title="Home" bg="black">
+      <Center w="100" h="100%" bg="black" textColor="white">
+        {hero_title}
+      </Center>
+      <Container>
+        <Center textColor="white">{hero_subtitle}</Center>
+      </Container>
     </PageContainer>
   )
 }
