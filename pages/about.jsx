34f111be050046a4d63808eec8216c4de7918294
Barclay <barclayiversen@gmail.com>
2022-02-02 21:29:27 -0800

    About page layout update

--- pages/about.jsx
+++ pages/about.jsx
@@ -1,21 +1,23 @@
 import PageContainer from '../components/PageContainer'
-import { Box, SimpleGrid, Image } from '@chakra-ui/react'
+import { Center, SimpleGrid, Image, Container } from '@chakra-ui/react'
 
 import dummyPhoto from '../public/group.jpg'
 
 export default function About() {
   return (
     <PageContainer title="About">
-      <SimpleGrid columns={{ md: 2 }} spacing="10px" px="2">
-        <Image w="100%" borderRadius="md" src="group.jpg" alt="group" />
-        <Image w="100%" borderRadius="md" src="group.jpg" alt="group" />
-        <Image w="100%" borderRadius="md" src="group.jpg" alt="group" />
-        <Image w="100%" borderRadius="md" src="group.jpg" alt="group" />
-      </SimpleGrid>
-      <Box textColor="white" fontSize="xl" p="4vh">
-        Dirty Not Sorry is an artist and promotion collective united by the
-        belief that music brings us together.
-      </Box>
+      <Container maxW="container.xl">
+        <SimpleGrid columns={{ sm: 2, lg: 3, '2xl': 4 }} spacing="10px" px="2">
+          <Image w="100%" borderRadius="md" src="group.jpg" alt="group" />
+          <Image w="100%" borderRadius="md" src="group.jpg" alt="group" />
+          <Image w="100%" borderRadius="md" src="group.jpg" alt="group" />
+          <Image w="100%" borderRadius="md" src="group.jpg" alt="group" />
+        </SimpleGrid>
+        <Center textColor="white" fontSize="xl" p="4vh">
+          Dirty Not Sorry is an artist and promotion collective united by the
+          belief that music brings us together.
+        </Center>
+      </Container>
     </PageContainer>
   )
 }
