77dc0e1d3e15288d24788c6f4192f1b061755096
Barclay <barclayiversen@gmail.com>
2022-02-07 15:44:50 -0800

    Adjust spacing, add Damasek + Mikey the lad

--- pages/artists.jsx
+++ pages/artists.jsx
@@ -7,16 +7,18 @@ export default function Artists() {
     <PageContainer title="Artists">
       <SimpleGrid
         columns={{ sm: 2, md: 3, lg: 4, xl: 5, '2xl': 6 }}
-        spacing="10px"
-        px="8"
+        spacingY="6"
+        spacingX="6"
         area="10"
+        px="2"
       >
         <Card djName="Knives" djPhoto="group.jpg" hoverPhoto="knives-7.png" />
         <Card djName="JaxxT" djPhoto="group.jpg" />
         <Card djName="CatLiu" djPhoto="group.jpg" />
         <Card djName="Glaas" djPhoto="group.jpg" />
         <Card djName="i2i" djPhoto="group.jpg" />
-        <Card djName="Aanster" djPhoto="group.jpg" />
+        <Card djName="Aanster & Mikey the Lad" djPhoto="group.jpg" />
+        <Card djName="Damasek" djPhoto="group.jpg" />
       </SimpleGrid>
     </PageContainer>
   )
