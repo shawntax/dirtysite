6b925b4f6805e5bd00944703225ce442a04b6c17
Barclay <barclayiversen@gmail.com>
2022-02-07 15:34:15 -0800

    Add white banner to page container. Change vh to % to fix white space

--- components/PageContainer.jsx
+++ components/PageContainer.jsx
@@ -1,13 +1,18 @@
-import { Heading, VStack } from '@chakra-ui/react'
+import { Heading, VStack, Box, Center } from '@chakra-ui/react'
 import NavBar from './NavBar'
 
 const PageContainer = ({ children, title }) => {
   return (
-    <VStack height="100vh" justify="between" pb="20" bg={'black'}>
+    <VStack height="100%" justify="between" pb="20" bg={'black'}>
       <NavBar />
-      <Heading as="h1" size="sm" color="red.100">
-        ({title})
-      </Heading>
+      <Box bg="gray.100" w="100%" py="6">
+        <Center>
+          <Heading as="h1" size="md" color="black">
+            {title}
+          </Heading>
+        </Center>
+      </Box>
+
       {children}
     </VStack>
   )
