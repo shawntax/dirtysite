8fa67aced55c5415e554b4fc99f797e46940aee3
Sumit Kataruka <sumitk6007@gmail.com>
2022-02-08 21:47:32 -0800

    background color fix

--- components/PageContainer.jsx
+++ components/PageContainer.jsx
@@ -5,7 +5,7 @@ import Footer from './Footer'
 const PageContainer = ({ children, title }) => {
   return (
     // weird white space on /artists and /about. Other pages are good.
-    <VStack height="100vh" justify="between" pb="20" bg={'black'}>
+    <VStack justify="between" pb="20" bg={'black'}>
       <NavBar />
       <Box bg="gray.100" w="100%" py="6">
         <Center>
