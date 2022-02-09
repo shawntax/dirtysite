39cc06c9fff39c3679ec116d5cd31b87034bc774
Barclay <barclayiversen@gmail.com>
2022-02-09 14:10:25 -0800

    Layout contact page

--- pages/contact.jsx
+++ pages/contact.jsx
@@ -1,5 +1,25 @@
 import PageContainer from '../components/PageContainer'
-
+import {
+  Container,
+  Text,
+  Input,
+  FormControl,
+  FormLabel,
+  FormErrorMessage,
+  FormHelperText,
+} from '@chakra-ui/react'
 export default function Contact() {
-  return <PageContainer title="Contact"></PageContainer>
+  return (
+    <PageContainer title="Contact">
+      <Container>
+        <Text textColor={'white'} fontSize="2xl" pb="4">
+          Join the DNS Family
+        </Text>
+        <FormLabel textColor={'white'}>Email</FormLabel>
+        <Input textColor={'white'}></Input>
+        <FormLabel textColor={'white'}>Phone</FormLabel>
+        <Input textColor={'white'}></Input>
+      </Container>
+    </PageContainer>
+  )
 }
