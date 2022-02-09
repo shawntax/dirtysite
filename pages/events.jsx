0fadc677897a1ce0c2b992b3fe333d6647a4bc7b
Barclay <barclayiversen@gmail.com>
2022-02-09 13:22:18 -0800

    Responsive event layout.

--- pages/events.jsx
+++ pages/events.jsx
@@ -1,5 +1,47 @@
 import PageContainer from '../components/PageContainer'
+import {
+  Box,
+  Text,
+  Center,
+  Grid,
+  GridItem,
+  Image,
+  Wrap,
+  WrapItem,
+  Divider,
+  Link,
+  Container,
+  Button,
+} from '@chakra-ui/react'
 
 export default function Events() {
-  return <PageContainer title="Events"></PageContainer>
+  return (
+    <PageContainer title="Events">
+      <Wrap w="80%" justify="center">
+        <WrapItem>
+          <Image src="sample.jpg" h="300px" w="240px" borderRadius="md" />
+        </WrapItem>
+        <Container>
+          <WrapItem>
+            <Box bg="gray.100" w="100%" p="4" borderRadius="md">
+              <Box truncate>
+                <Link textColor="blue">FOMO at Public works</Link>
+                <Text>Saturday April 20th 2024</Text>
+                <Divider orientation="horizontal" />
+                <Divider orientation="horizontal" />
+                <Divider orientation="horizontal" />
+                <Divider orientation="horizontal" />
+              </Box>
+              <Text pb="5">Content</Text>
+              <Center>
+                <Button textColor="gray.100" bg="gray.800">
+                  Get Tickets
+                </Button>
+              </Center>
+            </Box>
+          </WrapItem>
+        </Container>
+      </Wrap>
+    </PageContainer>
+  )
 }
