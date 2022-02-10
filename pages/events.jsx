4a6b480a1a7fcd7d950864326cc3fdecd18cc2fd
Barclay <barclayiversen@gmail.com>
2022-02-10 11:03:36 -0800

    Fix massive screw up in layout

--- pages/events.jsx
+++ pages/events.jsx
@@ -23,48 +23,44 @@ export default function Events() {
       <Wrap w="80%" justify="center" bg="" pb="10">
         {events.map((event) => {
           return (
-            <WrapItem>
-              <Image
-                src={event.attributes.featuredImage}
-                h="310px"
-                w="250px"
-                borderRadius="md"
-              />
-            </WrapItem>
+            <>
+              <WrapItem>
+                <Image
+                  src={event.attributes.featuredImage}
+                  h="310px"
+                  w="250px"
+                  borderRadius="md"
+                />
+              </WrapItem>
+              <Container>
+                <WrapItem>
+                  <Box bg="gray.100" w="100%" p="4" borderRadius="md">
+                    <Box truncate>
+                      <Link textColor="blue">{event.attributes.title}</Link>
+                      <Text>{event.attributes.eventDate}</Text>
+                      <Divider orientation="horizontal" />
+                      <Divider orientation="horizontal" />
+                      <Divider orientation="horizontal" />
+                      <Divider orientation="horizontal" />
+                    </Box>
+                    <Text pb="5">{event.attributes.body}</Text>
+                    <Center>
+                      <Link href={event.attributes.ticketLink}>
+                        <Button
+                          _hover={{ bg: 'gray.400', textColor: 'gray.800' }}
+                          textColor="gray.100"
+                          bg="gray.800"
+                        >
+                          Get Tickets
+                        </Button>
+                      </Link>
+                    </Center>
+                  </Box>
+                </WrapItem>
+              </Container>
+            </>
           )
         })}
-
-        <Container bg="">
-          {events.map((event) => {
-            console.log(event.attributes.content)
-            return (
-              <WrapItem>
-                <Box bg="gray.100" w="100%" p="4" borderRadius="md">
-                  <Box truncate>
-                    <Link textColor="blue">{event.attributes.title}</Link>
-                    <Text>{event.attributes.eventDate}</Text>
-                    <Divider orientation="horizontal" />
-                    <Divider orientation="horizontal" />
-                    <Divider orientation="horizontal" />
-                    <Divider orientation="horizontal" />
-                  </Box>
-                  <Text pb="5">{event.attributes.body}</Text>
-                  <Center>
-                    <Link href={event.attributes.ticketLink}>
-                      <Button
-                        _hover={{ bg: 'gray.400', textColor: 'gray.800' }}
-                        textColor="gray.100"
-                        bg="gray.800"
-                      >
-                        Get Tickets
-                      </Button>
-                    </Link>
-                  </Center>
-                </Box>
-              </WrapItem>
-            )
-          })}
-        </Container>
       </Wrap>
     </PageContainer>
   )
