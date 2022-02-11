5f9578eda2ec83ee37546091fa73d4afdc7388c5
Sumit Kataruka <sumitk6007@gmail.com>
2022-02-10 19:32:39 -0800

    Make nav button bigger

--- components/NavBar/MenuToggle.jsx
+++ components/NavBar/MenuToggle.jsx
@@ -5,7 +5,7 @@ import { CgMenuCake as MenuIcon, CgClose as CloseIcon } from 'react-icons/cg'
 const MenuToggle = ({ toggle, isOpen }) => {
   return (
     <Box display={{ base: 'block', md: 'none' }} onClick={toggle}>
-      {isOpen ? <CloseIcon /> : <MenuIcon />}
+      {isOpen ? <CloseIcon size={50}/> : <MenuIcon size={50} />}
     </Box>
   )
 }
