ddefd863c805046a8daad836df43216576282a8a
Shawn Taxerman <mail@shawn.tax>
2022-08-17 23:20:26 -0700

    add server form validation

--- components/Footer/MarketingForm.jsx
+++ components/Footer/MarketingForm.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import { useEffect } from 'react'
 import { useForm } from 'react-hook-form'
 import qs from 'qs'
 import axios from 'axios'
@@ -6,6 +6,7 @@ import safeAwait from 'safe-await'
 
 import {
   Box,
+  Text,
   Heading,
   FormErrorMessage,
   FormLabel,
@@ -18,7 +19,9 @@ export default function MarketingForm() {
   const {
     handleSubmit,
     register,
-    formState: { isSubmitting, errors },
+    formState: { isSubmitting, errors, isSubmitSuccessful },
+    setError,
+    reset,
   } = useForm()
 
   async function onSubmit(values) {
@@ -28,7 +31,7 @@ export default function MarketingForm() {
       data: JSON.stringify(values),
     }
 
-    const [error, { data }] = await safeAwait(
+    const [error, res] = await safeAwait(
       axios(
         `${process.env.NEXT_PUBLIC_FUNCTIONS_URL}/newsletter-signup`,
         options
@@ -36,82 +39,103 @@ export default function MarketingForm() {
     )
 
     if (error) {
-      console.error(error.message)
+      const {
+        response: { data: errorMessage },
+      } = error
+      setError('email', { type: 'server', errorMessage })
     } else {
-      console.log(data)
+      console.log(res)
     }
   }
+
+  /*   useEffect(() => {
+    if (isSubmitSuccessful) {
+      reset({ name: '', email: '' })
+    }
+  }, [isSubmitSuccessful, reset]) */
+
+  const EMAIL_REGEX = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+.[A-Z]{2,4}$/i
 
   return (
     <Box>
       <Heading as="h2" size="xl">
         Newsletter
       </Heading>
-      <form onSubmit={handleSubmit(onSubmit)} name="marketing-newsletter">
-        <FormControl>
-          <Input
-            type="text"
-            id="form_email"
-            name="username"
-            sx={{ display: 'none !important' }}
-            tabIndex="-1"
-            autocomplete="false"
-          />
-        </FormControl>
-        <FormControl isInvalid={errors.name}>
-          <FormLabel fontSize="2xl" htmlFor="name">
-            First name
-          </FormLabel>
-          <Input
-            id="name"
-            type="text"
-            size="lg"
-            htmlSize={10}
-            width="auto"
-            {...register('name', {
-              required: true,
-              minLength: {
-                value: 2,
-                message: 'Name must be at least 2 characters',
-              },
-              maxLength: {
-                value: 20,
-                message: 'Name cannot be more than 20 characters',
-              },
-            })}
-          />
-          <FormErrorMessage>
-            {errors.name && errors.name.message}
-          </FormErrorMessage>
-        </FormControl>
+      {isSubmitSuccessful ? (
+        <Text>Success! Check your inbox.</Text>
+      ) : (
+        <form onSubmit={handleSubmit(onSubmit)} name="marketing-newsletter">
+          <FormControl>
+            <Input
+              type="text"
+              id="form_email"
+              name="username"
+              sx={{ display: 'none !important' }}
+              tabIndex="-1"
+              autoComplete="false"
+              {...register('username', { required: false })}
+            />
+          </FormControl>
+          <FormControl isInvalid={errors.name}>
+            <FormLabel fontSize="2xl" htmlFor="name">
+              First name
+            </FormLabel>
+            <Input
+              id="name"
+              type="text"
+              size="lg"
+              htmlSize={10}
+              width="auto"
+              {...register('name', {
+                required: true,
+                minLength: {
+                  value: 2,
+                  message: 'Name must be at least 2 characters',
+                },
+                maxLength: {
+                  value: 20,
+                  message: 'Name cannot be more than 20 characters',
+                },
+              })}
+            />
+            <FormErrorMessage>
+              {errors.name && errors.name.message}
+            </FormErrorMessage>
+          </FormControl>
 
-        <FormControl isInvalid={errors.email}>
-          <FormLabel fontSize="2xl" htmlFor="email">
-            Email
-          </FormLabel>
-          <Input
-            id="email"
-            type="email"
-            size="lg"
-            htmlSize={20}
-            width="auto"
-            {...register('email', {
-              required: true,
-              pattern: {
-                value:
-                  /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-                message: 'Invalid email address',
-              },
-            })}
-          />
-          <FormErrorMessage>
-            {errors.email && errors.email.message}
-          </FormErrorMessage>
-        </FormControl>
-        <Button mt={4} color="black" isLoading={isSubmitting} type="submit">
-          Submit
-        </Button>
-      </form>
+          <FormControl isInvalid={errors.email}>
+            <FormLabel fontSize="2xl" htmlFor="email">
+              Email
+            </FormLabel>
+            <Input
+              id="email"
+              type="email"
+              size="lg"
+              htmlSize={20}
+              width="auto"
+              {...register('email', {
+                required: true,
+                pattern: {
+                  value: EMAIL_REGEX,
+                  message: 'Invalid email address',
+                },
+              })}
+            />
+            <FormErrorMessage>
+              {errors.email && errors.email.message}
+            </FormErrorMessage>
+          </FormControl>
+          <Button
+            mt={4}
+            bgColor="white"
+            color="black"
+            isLoading={isSubmitting}
+            type="submit"
+          >
+            Sign Up
+          </Button>
+        </form>
+      )}
     </Box>
   )
 }
--- functions/newsletter-signup.js
+++ functions/newsletter-signup.js
@@ -1,13 +1,28 @@
 const axios = require('axios')
 const safeAwait = require('safe-await')
+const { checkEmail } = require('email-validator-node')
 
 require('dotenv').config()
 
 exports.handler = async (event, context) => {
-  const submission = JSON.parse(event.body)
+  const { name, email, username } = JSON.parse(event.body)
 
-  console.log(submission)
+  if (username !== '') {
+    return {
+      statusCode: 200,
+      body: 'thanks, bot!',
+    }
+  }
 
+  const { isValid, message } = await checkEmail(email)
+
+  if (!isValid) {
+    console.log(message)
+    return {
+      statusCode: 400,
+      body: `Invalid email`,
+    }
+  }
   // const client = axios.create({
   // baseURL: 'https://dirty-not-sorry.myshopify.com/admin/api/2022-07/',
   // headers: {
@@ -17,6 +32,6 @@ exports.handler = async (event, context) => {
 
   return {
     statusCode: 200,
-    body: JSON.stringify(submission),
+    body: JSON.stringify({ name, email }),
   }
 }
