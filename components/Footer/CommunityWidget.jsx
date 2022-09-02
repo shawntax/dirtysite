a1a8b2592e878ae6f9866a6e84c22b03a67cec9f
shawn tax <mail@shawn.tax>
2022-09-01 21:42:06 -0700

    partytown wip

--- components/Footer/CommunityWidget.jsx
+++ components/Footer/CommunityWidget.jsx
@@ -1,25 +1,17 @@
 import React, { useState, useEffect } from 'react'
 import { Box } from '@chakra-ui/react'
 import Script from 'next/script'
+import { useRouter } from 'next/router'
 
 const CommunityWidget = () => {
-  const [hasMounted, setHasMounted] = useState(false)
-
-  useEffect(() => {
-    setHasMounted(true)
-  }, [])
-
-  if (!hasMounted) {
-    return null
-  }
-
   return (
     <>
+      <p>ay</p>
       <Box id="community-embed" w="100%" />
       <Script
         src="https://mgu-embed.community.com/embed.js"
         data-community-embed-id="8f9f5079-99cf-451f-b2fc-8baafc2a652d"
-        strategy="lazyOnload"
+        strategy="worker"
       />
     </>
   )
--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@ const withPlugins = require('next-compose-plugins')
 const withOptimizedImages = require('next-optimized-images')
 
 const nextConfig = {
+  experimental: {
+    nextScriptWorkers: true,
+  },
   images: {
     disableStaticImages: true,
   },
