bd01e0c8289247a936301c59aac88783061a6e83
Barclay <barclayiversen@gmail.com>
2022-09-26 18:12:45 -0700

    Conditional rendering for social links

--- pages/artists/[slug].jsx
+++ pages/artists/[slug].jsx
@@ -2,7 +2,16 @@ import { getLayout } from '@components/SiteLayout'
 import { Flex, Box, Heading, VStack, Text, AspectRatio } from '@chakra-ui/react'
 import Photo from '@components/Photo'
 import NCLink from '@components/NCLink'
-import { SiFacebook, SiInstagram, SiSoundcloud } from 'react-icons/si'
+import {
+  SiFacebook,
+  SiInstagram,
+  SiMixcloud,
+  SiSoundcloud,
+  SiSpotify,
+  SiTiktok,
+  SiTwitch,
+  SiTwitter,
+} from 'react-icons/si'
 import { fetchArtists } from '@helpers/cms.helpers'
 
 function Artist({ artist }) {
@@ -31,24 +40,93 @@ function Artist({ artist }) {
       <Flex direction="column" my={{ base: '4', lg: 'none' }}>
         <Heading>{artist.name}</Heading>
         <VStack fontSize="2xl" align="start" spacing="0" mt="4">
-          <NCLink to={artist.fbLink}>
-            <Flex align="center" spacing="2">
-              <SiFacebook />
-              <Text pl="2">Facebook</Text>
-            </Flex>
-          </NCLink>
-          <NCLink to={artist.instaLink}>
-            <Flex align="center" spacing="2">
-              <SiInstagram />
-              <Text pl="2">Instagram</Text>
-            </Flex>
-          </NCLink>
-          <NCLink to={artist.scLink}>
-            <Flex align="center" spacing="2">
-              <SiSoundcloud />
-              <Text pl="2">SoundCloud</Text>
-            </Flex>
-          </NCLink>
+          {artist.fbLink ? (
+            <NCLink to={artist.fbLink}>
+              <Flex align="center" spacing="2">
+                <SiFacebook />
+                <Text pl="2">Facebook</Text>
+              </Flex>
+            </NCLink>
+          ) : (
+            <></>
+          )}
+
+          {artist.instaLink ? (
+            <NCLink to={artist.instaLink}>
+              <Flex align="center" spacing="2">
+                <SiInstagram />
+                <Text pl="2">Instagram</Text>
+              </Flex>
+            </NCLink>
+          ) : (
+            <></>
+          )}
+
+          {artist.scLink ? (
+            <NCLink to={artist.scLink}>
+              <Flex align="center" spacing="2">
+                <SiSoundcloud />
+                <Text pl="2">SoundCloud</Text>
+              </Flex>
+            </NCLink>
+          ) : (
+            <></>
+          )}
+
+          {artist.tiktokLink ? (
+            <NCLink to={artist.tiktokLink}>
+              <Flex align="center" spacing="2">
+                <SiTiktok />
+                <Text pl="2">TikTok</Text>
+              </Flex>
+            </NCLink>
+          ) : (
+            <></>
+          )}
+
+          {artist.twitchLink ? (
+            <NCLink to={artist.twitchLink}>
+              <Flex align="center" spacing="2">
+                <SiTwitch />
+                <Text pl="2">Twitch</Text>
+              </Flex>
+            </NCLink>
+          ) : (
+            <></>
+          )}
+
+          {artist.twitterLink ? (
+            <NCLink to={artist.twitterLink}>
+              <Flex align="center" spacing="2">
+                <SiTwitter />
+                <Text pl="2">Twitter</Text>
+              </Flex>
+            </NCLink>
+          ) : (
+            <></>
+          )}
+
+          {artist.mcLink ? (
+            <NCLink to={artist.mcLink}>
+              <Flex align="center" spacing="2">
+                <SiMixcloud />
+                <Text pl="2">Mixcloud</Text>
+              </Flex>
+            </NCLink>
+          ) : (
+            <></>
+          )}
+
+          {artist.spotLink ? (
+            <NCLink to={artist.spotLink}>
+              <Flex align="center" spacing="2">
+                <SiSpotify />
+                <Text pl="2">Spotify</Text>
+              </Flex>
+            </NCLink>
+          ) : (
+            <></>
+          )}
         </VStack>
       </Flex>
     </Flex>
