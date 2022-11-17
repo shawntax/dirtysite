370a322f4da9df366802e0ae722e415525e5f3a7
shawn tax <mail@shawn.tax>
2022-11-16 22:02:33 -0800

    switch footer component on viewport

--- components/Footer/Footer.jsx
+++ components/Footer/Footer.jsx
@@ -1,33 +1,37 @@
 import React from 'react'
 import { Text, Stack, StackDivider, Flex, Center, Box } from '@chakra-ui/react'
-// import CommunityWidget from './CommunityWidget'
+import CommunityWidget from './CommunityWidget'
 import MarketingForm from './MarketingForm'
+import { useMediaQuery } from '@hooks/useMediaQuery'
 
-const Footer = () => (
-  <Flex
-    w="full"
-    direction="column"
-    justify="space-between"
-    px={{ base: 0, sm: '4' }}
-    pt="10"
-    borderTop="1px"
-    borderTopColor="gray.900"
-  >
-    <Stack
-      direction={['column', 'row']}
-      divider={<StackDivider borderColor="gray.900" />}
+const Footer = () => {
+  const isLargerThan600px = useMediaQuery(600)
+
+  return (
+    <Flex
+      w="full"
+      direction="column"
+      justify="space-between"
+      px={{ base: 0, sm: '4' }}
+      pt="10"
+      borderTop="1px"
+      borderTopColor="gray.900"
     >
-      <Box w="full" px="8">
-        {/* <CommunityWidget /> */}
-        <MarketingForm />
-      </Box>
-    </Stack>
-    <Center>
-      <Text pt={16} pb={4} color="white" mx="auto">
-        © 2022 Dirty Not Sorry
-      </Text>
-    </Center>
-  </Flex>
-)
+      <Stack
+        direction={['column', 'row']}
+        divider={<StackDivider borderColor="gray.900" />}
+      >
+        <Box w="full" px="8">
+          {isLargerThan600px ? <MarketingForm /> : <CommunityWidget />}
+        </Box>
+      </Stack>
+      <Center>
+        <Text pt={16} pb={4} color="white" mx="auto">
+          © 2022 Dirty Not Sorry
+        </Text>
+      </Center>
+    </Flex>
+  )
+}
 
 export default Footer
