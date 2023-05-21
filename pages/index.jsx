552b6a1f7292baec83c345ee284d609777dc059b
shawn tax <mail@shawn.tax>
2023-05-20 17:32:34 -0700

    tidy up events grid on home

--- pages/index.jsx
+++ pages/index.jsx
@@ -3,7 +3,6 @@ import { getLayout } from '@components/HomeLayout'
 import EventCard from '@components/EventList/EventCard'
 import PageHeader from '@components/PageHeader'
 import Hero from '@components/Hero'
-import EventList from '@components/EventList'
 import { attributes } from '@content/pages/home.md'
 import { react as AboutContent } from '@content/pages/about.md'
 import { fetchEvents } from '@helpers/cms.helpers'
@@ -18,10 +17,12 @@ export default function Home({ upcomingLiveEvents, upcomingStreams }) {
         id="about"
         as="section"
         centerContent
-        maxW={{ base: 'container.lg', xl: 'container.lg' }}
+        maxW="container.lg"
         py={{ base: '12', lg: '24' }}
       >
         <Box
+          id="about"
+          as="section"
           fontSize="3xl"
           maxW="container.lg"
           py={{ base: '2', lg: '12' }}
@@ -29,44 +30,36 @@ export default function Home({ upcomingLiveEvents, upcomingStreams }) {
         >
           <AboutContent />
         </Box>
-
-        {/* {upcomingLiveEvents.length > 0 && (
-          <EventList title="Upcoming Events" events={upcomingLiveEvents} />
+      </Container>
+      <Container maxW="container.xl" py="2">
+        {upcomingLiveEvents.length > 0 && (
+          <>
+            <Heading as="h1" textTransform="capitalize" w="fit" py="12">
+              Upcoming Events
+            </Heading>
+            <SimpleGrid minChildWidth="300px" spacing="10">
+              {upcomingLiveEvents.map((event) => {
+                {
+                  return <EventCard key={event.id} event={event} />
+                }
+              })}
+            </SimpleGrid>
+          </>
         )}
         {upcomingStreams.length > 0 && (
-          <EventList title="Upcoming Streams" events={upcomingStreams} />
-        )} */}
-        <Container maxW="container.lg">
-          {upcomingLiveEvents.length > 0 && (
-            <>
-              <Heading as="h1" textTransform="capitalize" w="fit" py="4">
-                Upcoming Events
-              </Heading>
-              <SimpleGrid minChildWidth="250px" spacing="10" mt="5">
-                {upcomingLiveEvents.map((event) => {
-                  {
-                    return <EventCard key={event.id} event={event} />
-                  }
-                })}
-              </SimpleGrid>
-            </>
-          )}
-
-          {upcomingStreams.length > 0 && (
-            <>
-              <Heading as="h1" textTransform="capitalize" w="fit" py="4">
-                Upcoming Streams
-              </Heading>
-              <SimpleGrid minChildWidth="250px" spacing="10" mt="5">
-                {upcomingStreams.map((event) => {
-                  {
-                    return <EventCard key={event.id} event={event} />
-                  }
-                })}
-              </SimpleGrid>
-            </>
-          )}
-        </Container>
+          <>
+            <Heading as="h1" textTransform="capitalize" w="fit" py="12">
+              Upcoming Streams
+            </Heading>
+            <SimpleGrid minChildWidth="250px" spacing="10">
+              {upcomingStreams.map((event) => {
+                {
+                  return <EventCard key={event.id} event={event} />
+                }
+              })}
+            </SimpleGrid>
+          </>
+        )}
       </Container>
     </>
   )
