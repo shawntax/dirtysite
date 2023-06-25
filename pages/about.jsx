3ef8e52cb264e36fd69c91cee79fbf810385507f
Shawn Taxerman <mail@shawn.tax>
2023-06-25 14:03:44 -0700

    add markdown and fix copy (#18)
    Co-authored-by: Shawn Taxerman <shawn@retool.com>

--- pages/about.jsx
+++ pages/about.jsx
@@ -1,13 +1,31 @@
 import { getLayout } from '@components/SiteLayout'
-import { Box, SimpleGrid, Heading, Text } from '@chakra-ui/react'
+import {
+  Container,
+  Flex,
+  Box,
+  SimpleGrid,
+  Heading,
+  Text,
+} from '@chakra-ui/react'
 import PageHeader from '@components/PageHeader'
 import Photo from '@components/Photo'
 import { attributes, react as AboutContent } from '@content/pages/about.md'
+import ChakraUIRenderer from 'chakra-ui-markdown-renderer'
+import ReactMarkdown from 'react-markdown'
 
+const aboutTheme = {
+  p: ({ children }) => {
+    return (
+      <Text fontSize="3xl" py="2">
+        {children}
+      </Text>
+    )
+  },
+}
 export default function About() {
   const { seoDesc, missionStatement, values, identity } = attributes
   return (
-    <>
+    <Container maxW="container.lg">
       <PageHeader title="About" seoDesc={seoDesc}>
         About
       </PageHeader>
@@ -16,43 +34,60 @@ export default function About() {
           <AboutContent />
         </Box>
       )}
-      {/* the length of any empty markdown input is \n\n\n */}
-      {identity.length > 3 && (
-        <Box>
-          <Heading as="h1" textTransform="capitalize" w="fit" py="4">
-            Identity
-          </Heading>
-          <Text fontSize="3xl">{identity}</Text>
-        </Box>
-      )}
-      {missionStatement > 3 && (
-        <Box>
-          <Heading as="h1" textTransform="capitalize" w="fit" py="4">
-            Mission Statement
-          </Heading>
-          <Text fontSize="3xl">{missionStatement}</Text>
-        </Box>
-      )}
-      {values > 3 && (
-        <Box>
-          <Heading as="h1" textTransform="capitalize" w="fit" py="4">
-            values
-          </Heading>
-          <Text fontSize="3xl">{values}</Text>
-        </Box>
-      )}
-      <SimpleGrid columns={{ base: 1, lg: 2 }} mt="4" spacing="8">
-        <Photo
-          fileName="group-acraze.jpg"
-          alt="group"
-          height={500}
-          width={500}
-        />
-        <Photo fileName="group.jpg" alt="group" height={500} width={500} />
-        <Photo fileName="group-2.jpg" alt="group" height={500} width={500} />
-        <Photo fileName="group-4.jpg" alt="group" height={500} width={500} />
-      </SimpleGrid>
-    </>
+      <Flex direction="column">
+        {/* the length of any empty markdown input is \n\n\n */}
+
+        {missionStatement.length > 3 && (
+          <Box my="4">
+            <Heading as="h1" textTransform="capitalize" w="fit" py="4">
+              Mission Statement
+            </Heading>
+            <ReactMarkdown
+              components={ChakraUIRenderer(aboutTheme)}
+              children={missionStatement}
+              skipHtml
+            />
+          </Box>
+        )}
+
+        {identity.length > 3 && (
+          <Box my="4">
+            <Heading as="h1" textTransform="capitalize" w="fit" py="4">
+              Identity
+            </Heading>
+            <ReactMarkdown
+              components={ChakraUIRenderer(aboutTheme)}
+              children={identity}
+              skipHtml
+            />
+          </Box>
+        )}
+
+        {values.length > 3 && (
+          <Box my="4">
+            <Heading as="h1" textTransform="capitalize" w="fit" py="4">
+              values
+            </Heading>
+            <ReactMarkdown
+              components={ChakraUIRenderer(aboutTheme)}
+              children={values}
+              skipHtml
+            />
+          </Box>
+        )}
+        <SimpleGrid columns={{ base: 1, lg: 2 }} mt="4" spacing="8">
+          <Photo
+            fileName="group-acraze.jpg"
+            alt="group"
+            height={500}
+            width={500}
+          />
+          <Photo fileName="group.jpg" alt="group" height={500} width={500} />
+          <Photo fileName="group-2.jpg" alt="group" height={500} width={500} />
+          <Photo fileName="group-4.jpg" alt="group" height={500} width={500} />
+        </SimpleGrid>
+      </Flex>
+    </Container>
   )
 }
 About.pageName = 'About'
