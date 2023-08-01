b5a6000030b2d09dcb7ceb380bee3cc8065c0e6e
Shawn Taxerman <shawn@retool.com>
2023-07-31 22:00:25 -0700

    rm more pixel stuff

--- helpers/pixel.helpers.js
+++
@@ -1,5 +0,0 @@
-export const FB_PIXEL_ID = process.env.NEXT_PUBLIC_FACEBOOK_PIXEL_ID
-
-export const pageView = () => {
-  window.fbq('track', 'PageView')
-}
--- pages/_document.jsx
+++ pages/_document.jsx
@@ -1,5 +1,4 @@
 import NextDocument, { Html, Head, Main, NextScript } from 'next/document'
-import { FB_PIXEL_ID } from '@helpers/pixel.helpers'
 
 export default class MyDocument extends NextDocument {
   static async getInitialProps(ctx) {
@@ -13,15 +12,6 @@ export default class MyDocument extends NextDocument {
         <Head>
           <meta charSet="UTF-8" />
           <meta content="ie=edge" httpEquiv="X-UA-Compatible" />
-          <noscript>
-            {/*  eslint-disable-next-line */}
-            <img
-              height="1"
-              width="1"
-              style={{ display: 'none' }}
-              src={`https://www.facebook.com/tr?id=${FB_PIXEL_ID}&ev=PageView&noscript=1`}
-            />
-          </noscript>
         </Head>
 
         <body>
