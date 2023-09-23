8d423c3dadf7d4236aa033056c7d234555e61947
Shawn Taxerman <shawn@retool.com>
2023-09-22 19:41:56 -0700

    hacky compare

--- components/EventList/EventCard.jsx
+++ components/EventList/EventCard.jsx
@@ -34,7 +34,9 @@ const EventCard = ({ event }) => {
                 {title}
               </Text>
               <Text textColor="gray.400" fontSize={{ base: '2xl', xl: '3xl' }}>
-                {dayjs(event.eventDate).format('M/D')}
+                {event.id === 's_hUxFDDGm'
+                  ? dayjs(event.eventDate).subtract(1, 'day').format('M/D')
+                  : dayjs(event.eventDate).format('M/D')}
               </Text>
             </Flex>
           </NCLink>
