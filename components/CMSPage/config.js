c70e13f5714312e43912eae0c38952ade725bd2f
Shawn Taxerman <shawn@retool.com>
2023-10-04 21:38:35 -0700

    revert date migration; remove test event

--- components/CMSPage/config.js
+++ components/CMSPage/config.js
@@ -185,7 +185,7 @@ const collections = [
         widget: 'datetime',
         date_format: 'LLLL d yyyy',
         time_format: false,
-        format: "yyyy-MM-dd'T'HH:mm:ssXXX",
+        format: "yyyy-MM-dd'T'HH:mm:ss.SSSXXX",
       },
       {
         label: 'Event Location',
