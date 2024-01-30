a8c2fae1b0890cf141d412f9d7fe034d025f623c
Shawn Taxerman <shawn@retool.com>
2024-01-29 22:06:51 -0800

    its 2024 baybee

--- components/Footer/Footer.jsx
+++ components/Footer/Footer.jsx
@@ -27,7 +27,7 @@ const Footer = () => {
       </Stack>
       <Center>
         <Text pt={16} pb={4} color="white" mx="auto">
-          © 2023 Dirty Not Sorry
+          © 2024 Dirty Not Sorry
         </Text>
       </Center>
     </Flex>
